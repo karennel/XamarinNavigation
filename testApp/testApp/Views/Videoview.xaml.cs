0b0dd7bbf59d4242dc52bb014340c31594fed174
Karen Nel <nelk@out-bateleur.co.za>
2020-10-13 14:35:35 +0200

    * new changes for video view

---
+++ testApp/testApp/Views/Videoview.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace testApp.Views
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class Videoview : ContentPage
+  {
+    public Videoview()
+    {
+      InitializeComponent();
+    }
+  }
+}
